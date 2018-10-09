a7b30314749a250c5b907b0d29fd71c7aad3611d
Matt Haggard <haggardii@gmail.com>
2018-10-09 16:52:42 -0600

    Proc echos hi

--- src/argparse.nim
+++ src/argparse.nim
@@ -95,7 +95,8 @@ proc generateReturnType(builder: var Builder): NimNode {.compileTime.} =
   result = objdef.root
 
 proc generateParseProc(builder: var Builder): NimNode {.compileTime.} =
-  var body = newStmtList()
+  var body = quote do:
+    echo "hi!"
   result = newNimNode(nnkProcDef).add(
     ident("parse"),
     newEmptyNode(),
@@ -164,7 +165,7 @@ proc mkParser*(name: string, content: proc()): NimNode {.compileTime.} =
   result.add(parser.root)
 
   # Create the parse proc
-  # result.add(builder.generateParseProc())
+  result.add(builder.generateParseProc())
 
   # Instantiate a parser and return an instance
   result.add(builder.instantiateParser())
