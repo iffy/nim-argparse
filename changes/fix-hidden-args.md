1c34f1e54175b0ac35d997be5475b5ecffdb8763
Matt Haggard <haggardii@gmail.com>
2020-11-27 22:29:26 -0500

    Add docs back and restore hidden args

---
+++ changes/fix-hidden-args.md
@@ -0,0 +1,1 @@
+Support hidden args again.
--- src/argparse.nim
+++ src/argparse.nim
@@ -1,3 +1,57 @@
+## argparse is an explicit, strongly-typed command line argument parser.
+##
+## Use ``newParser`` to create a parser.  Within the body
+## of the parser use the following procs/templates (read the individual
+## documentation for more details):
+##
+## - ``flag`` - for boolean flags (e.g. ``--dryrun``)
+## - ``option`` - for options which have arguments (e.g. ``--output foo``)
+## - ``arg`` - for positional arguments (e.g. ``file1 file2``)
+## - ``command`` - for sub commands
+## - ``run`` - to define code to run when the parser is used in run mode rather than parse mode.  See the documentation for more information.
+## - ``help`` - to define a help string for the parser/subcommand
+## - ``nohelpflag`` - to disable the automatic `-h/--help` flag.  When using ``parse()``, ``.help`` will be a boolean flag.
+##
+## The following special variables are available within ``run`` blocks:
+##
+## - ``opts`` - contains your user-defined options
+## - ``opts.parentOpts`` - a reference to parent options (i.e. from a subcommand)
+## - ``opts.argparseCommand`` - a string holding the chosen command
+##
+## If ``Parser.parse()`` and ``Parser.run()`` are called without arguments, they use the arguments from the command line.
+## 
+runnableExamples:
+  var p = newParser("My Program"):
+    help("A description of this program")
+    flag("-n", "--dryrun")
+    option("-o", "--output", help="Write output to this file", default=some("somewhere.txt"))
+    option("-k", "--kind", choices = @["fruit", "vegetable"])
+    arg("input")
+  
+  let opts = p.parse(@["-n", "--output", "another.txt", "cranberry"])
+  assert opts.dryrun == true
+  assert opts.output == "another.txt"
+  assert opts.input == "cranberry"
+
+runnableExamples:
+  var res:string
+  var p = newParser("Something"):
+    flag("-n", "--dryrun")
+    command("ls"):
+      run:
+        res = "did ls"
+    command("run"):
+      option("-c", "--command")
+      run:
+        if opts.parentOpts.dryrun:
+          res = "would have run: " & opts.command
+        else:
+          res = "ran " & opts.command
+  
+  p.run(@["-n", "run", "--command", "something"])
+  assert res == "would have run: something"
+
+
 import macros; export macros
 import strutils
 import argparse/backend; export backend
@@ -9,6 +63,8 @@ proc toVarname(x: string): string =
   x.replace("-", "_").strip(chars={'_'})
 
 proc longAndShort(name1: string, name2: string): tuple[long: string, short: string] =
+  ## Given two strings, return the longer and shorter of the two with
+  ## shortname possibly being empty.
   var
     longname: string
     shortname: string
@@ -23,8 +79,41 @@ proc longAndShort(name1: string, name2: string): tuple[long: string, short: stri
       shortname = name1
   return (longname, shortname)
 
-proc flag*(name1: string, name2 = "", multiple = false, help = "") {.compileTime.} =
-  ## Add a boolean flag to this parser
+template newParser*(name: string, body: untyped): untyped =
+  ## Entry point for making command-line parsers.
+  ##
+  runnableExamples:
+    var p = newParser("My program"):
+      flag("-a")
+    assert p.parse(@["-a"]).a == true
+
+  macro domkParser(): untyped =
+    let builder = addParser(name, "", proc() = body)
+    builder.generateDefs()
+  domkParser()
+
+proc flag*(name1: string, name2 = "", multiple = false, help = "", hidden = false) {.compileTime.} =
+  ## Add a boolean flag to the argument parser.  The boolean
+  ## will be available on the parsed options object as the
+  ## longest named flag.
+  ##
+  ## If ``multiple`` is true then the flag can be specified multiple
+  ## times and the datatype will be an int.
+  ##
+  ## If ``hidden`` is true then the flag usage is not shown in the help.
+  ##
+  ## ``help`` is additional help text for this flag.
+  runnableExamples:
+    var p = newParser("Some Thing"):
+      flag("--show-name", help="Show the name")
+      flag("-a", help="Some flag named a")
+      flag("-n", "--dryrun", help="Don't actually run")
+    
+    let opts = p.parse(@["--show-name", "-n"])
+    assert opts.show_name == true
+    assert opts.a == false
+    assert opts.dryrun == true
+
   let names = longAndShort(name1, name2)
   let varname = names.long.toVarname()
   builderStack[^1].components.add Component(
@@ -34,15 +123,36 @@ proc flag*(name1: string, name2 = "", multiple = false, help = "") {.compileTime
     flagShort: names.short,
     flagLong: names.long,
     flagMultiple: multiple,
+    hidden: hidden,
   )
 
-proc option*(name1: string, name2 = "", help = "", default = none[string](), env = "", multiple = false, choices: seq[string] = @[]) {.compileTime.} =
-  ## Add an option to this parser
+proc option*(name1: string, name2 = "", help = "", default = none[string](), env = "", multiple = false, choices: seq[string] = @[], hidden = false) {.compileTime.} =
+  ## Add an option to the argument parser.  The longest
+  ## named flag will be used as the name on the parsed
+  ## result.
+  ##
+  ## Set ``multiple`` to true to accept multiple options.
+  ##
+  ## Set ``default`` to the default string value.
+  ##
+  ## Set ``env`` to an environment variable name to use as the default value
+  ## 
+  ## Set ``choices`` to restrict the possible choices.
+  ##
+  ## Set ``hidden`` to prevent the option usage listing in the help text.
+  ##
+  ## ``help`` is additional help text for this option.
+  runnableExamples:
+    var p = newParser("Command"):
+      option("-a", "--apple", help="Name of apple")
+    assert p.parse(@["-a", "5"]).apple == "5"
+
   let names = longAndShort(name1, name2)
   let varname = names.long.toVarname()
   builderStack[^1].components.add Component(
     kind: ArgOption,
     help: help,
+    hidden: hidden,
     varname: varname,
     env: env,
     optShort: names.short,
@@ -53,8 +163,30 @@ proc option*(name1: string, name2 = "", help = "", default = none[string](), env
   )
 
 proc arg*(varname: string, default = none[string](), env = "", help = "", nargs = 1) {.compileTime.} =
-  ## Add place-value argument to this parser
+  ## Add an argument to the argument parser.
+  ##
+  ## Set ``default`` to the default Option[string] value.  This is only
+  ## allowed for ``nargs = 1``.
+  ##
+  ## Set ``env`` to an environment variable name to use as the default value. This is only allowed for ``nargs = 1``.
   ## 
+  ## The value ``nargs`` has the following meanings:
+  ## 
+  ## - ``nargs = 1`` : A single argument. The value type will be ``string``
+  ## - ``nargs = 2`` (or more) : Accept a specific number of arguments.  The value type will be ``seq[string]``
+  ## - ``nargs = -1`` : Accept 0 or more arguments. Only one ``nargs = -1`` ``arg()`` is allowed per parser/command.
+  ##
+  ## ``help`` is additional help text for this argument.
+  runnableExamples:
+    var p = newParser("Command"):
+      arg("name", help = "Name of apple")
+      arg("twowords", nargs = 2)
+      arg("more", nargs = -1)
+    let res = p.parse(@["cameo", "hot", "dog", "things"])
+    assert res.name == "cameo"
+    assert res.twowords == @["hot", "dog"]
+    assert res.more == @["things"]
+
   builderStack[^1].components.add Component(
     kind: ArgArgument,
     help: help,
@@ -65,15 +197,33 @@ proc arg*(varname: string, default = none[string](), env = "", help = "", nargs 
   )
 
 proc help*(helptext: string) {.compileTime.} =
-  ## Add help to this parser/subcommand
+  ## Add help to a parser or subcommand.
+  ##
+  runnableExamples:
+    var p = newParser("Some Program"):
+      help("Some helpful description")
+      command("dostuff"):
+        help("More helpful information")
+    echo p.help
+  
   builderStack[^1].help &= helptext
 
 proc nohelpflag*() {.compileTime.} =
   ## Disable the automatic -h/--help flag
+  runnableExamples:
+    var p = newParser("Some Thing"):
+      nohelpflag()
+
   builderStack[^1].components.del(0)
 
 template run*(body: untyped): untyped =
   ## Add a run block to this command
+  runnableExamples:
+    var p = newParser("Some Program"):
+      command("dostuff"):
+        run:
+          echo "Actually do stuff"
+
   add_runproc(replaceNodes(quote(body)))
 
 template command*(name: string, group: string, content: untyped): untyped =
@@ -82,11 +232,14 @@ template command*(name: string, group: string, content: untyped): untyped =
     content
 
 template command*(name: string, content: untyped): untyped =
-  ## Add a subcommand to this parser
+  ## Add a sub-command to the argument parser.
+  ##
+  ## group is a string used to group commands in help output
+  runnableExamples:
+    var p = newParser("Some Program"):
+      command("dostuff"):
+        run:
+          echo "Actually do stuff"
+    p.run(@["dostuff"])
   command(name, "", content)
 
-template newParser*(name: string, body: untyped): untyped =
-  macro domkParser(): untyped =
-    let builder = addParser(name, "", proc() = body)
-    builder.generateDefs()
-  domkParser()
